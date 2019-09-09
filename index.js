8373150aaac3d0af0e7e10b59efd4a948c2a63dd
rishabh <rishabhmittal200@gmail.com>
2019-09-10 03:09:31 +0530

    Added code for manual authentication

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+const db = require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assets/'));
+app.use(expressLayouts);
 //use express router
 app.use('/',require("./routes/"));
 // set the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//extract styles and scripts from partials into layout header
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts',true);
 
 //start and listen to server
 app.listen(port, function(err){
